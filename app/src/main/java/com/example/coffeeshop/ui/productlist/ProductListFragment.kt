0a7569598952338c27f99a9c4bf27848b62d8789
BlankaMZ <blanka.maria.zadzinska@gmail.com>
2021-10-21 15:35:33 +0200

    adding UI for the searching

--- app/src/main/java/com/example/coffeeshop/ui/productlist/ProductListFragment.kt
+++ app/src/main/java/com/example/coffeeshop/ui/productlist/ProductListFragment.kt
@@ -1,15 +1,22 @@
 package com.example.coffeeshop.ui.productlist
 
+import android.app.SearchManager
+import android.content.Context
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.util.Log
+import android.view.*
+import androidx.appcompat.widget.SearchView
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
+import com.example.coffeeshop.R
 import com.example.coffeeshop.databinding.FragmentProductListBinding
 import com.example.coffeeshop.ui.BaseFragment
 import com.example.coffeeshop.ui.productlist.ProductListAdapter.ProductListOnclickListener
 import javax.inject.Inject
+import androidx.core.view.MenuItemCompat
+import com.example.coffeeshop.ui.MainActivity
+
 
 class ProductListFragment : BaseFragment() {
 
@@ -18,6 +25,31 @@ class ProductListFragment : BaseFragment() {
     private val viewModel by viewModels<ProductListViewModel> { viewModelFactoryProvider }
 
     private lateinit var productListAdapter: ProductListAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.product_list_search_menu, menu)
+
+        val searchItem = menu.findItem(R.id.action_search)
+        val searchView = searchItem.actionView as SearchView
+        searchView.queryHint = getString(R.string.search_hint)
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(p0: String?): Boolean {
+                return true
+            }
+
+            override fun onQueryTextChange(text: String?): Boolean {
+                Log.i("Query change", "Written text: $text")
+                return true
+            }
+        })
+
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
